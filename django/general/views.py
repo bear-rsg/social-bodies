411d256d309c0b5e3e2fe99a3092082e9c82cbbb
Michael Allaway <m.j.allaway@bham.ac.uk>
2021-03-22 13:04:44 +0000

    added the 3 new pages and removed coming soon

--- django/general/views.py
+++ django/general/views.py
@@ -1,3 +1,29 @@
-# This app only includes basic TemplateView generic views.
-# These are included in urls.py, within the urlpatterns array.
-# This is why this views.py file is empty.
+from django.views.generic import TemplateView
+
+
+class WelcomeTemplateView(TemplateView):
+    """
+    Welcome-based view to show the welcome template
+    """
+    template_name = 'general/welcome.html'
+
+
+class AboutTemplateView(TemplateView):
+    """
+    Class-based view to show the aboutrsno template
+    """
+    template_name = 'general/about.html'
+
+
+class BlogTemplateView(TemplateView):
+    """
+    Class-based view to show the blog template
+    """
+    template_name = 'general/blog.html'
+
+
+class CookiesTemplateView(TemplateView):
+    """
+    Class-based view to show the cookies template
+    """
+    template_name = 'general/cookies.html'
