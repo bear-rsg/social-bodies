a3cf9787857abeb1525c2b6f09d837e13fcdf805
Mike Allaway <36171746+mike-allaway@users.noreply.github.com>
2022-10-13 11:22:36 +0100

    removed magnifying glass

--- django/core/static/js/transcription.js
+++ django/core/static/js/transcription.js
@@ -1,33 +1,5 @@
 $(document).ready(function() {
 
-    // Image magnifying glass
-    $('.transcription-images-image-container').on('mousemove', function(e){
-        console.log(1)
-        var this_main_img_id = $(this).find('.main-img').attr('id'),
-        this_zoomed_img_id = $(this).find('.zoomed-img').attr('id'),
-            original = document.getElementById(this_main_img_id),
-            magnified = document.getElementById(this_zoomed_img_id),
-            style = magnified.style,
-            x = e.pageX - this.offsetLeft,
-            y = e.pageY - this.offsetTop,
-            imgWidth = original.width,
-            imgHeight = original.height,
-            xperc = (x / imgWidth) * 100,
-            yperc = (y / imgHeight) * 100;
-        // Add some margin for right edge
-        if (x > 0.01 * imgWidth) xperc += 0.15 * xperc;
-        // Add some margin for bottom edge
-        if (y >= 0.01 * imgHeight) yperc += 0.15 * yperc;
-        // Set the background of the magnified image horizontal
-        style.backgroundPositionX = xperc - 9 + '%';
-        // Set the background of the magnified image vertical
-        style.backgroundPositionY = yperc - 9 + '%';
-        // Move the magnifying glass with the mouse movement.
-        var width_height_half = 125; // whatever value here (e.g. 125) must be double in CSS width & height (e.g. 250px)
-        style.left = x - width_height_half + 'px';
-        style.top = y - width_height_half + 'px';
-    });
-
     // Transcription text switches
     $('#transcription-texts-switch div').on('click', function(){
         // Make switch active/inactive
