8ef4220d5371173e89a3bf284b8b70df7b426a60
Mike Allaway <36171746+mike-allaway@users.noreply.github.com>
2022-10-13 12:12:56 +0100

    added datamigration to trigger save method on all letterimage objects

---
+++ django/researchdata/migrations/0009_letterimage_datamigration.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+from researchdata import models
+
+
+def trigger_save_letterimage(apps, schema_editor):
+    """
+    Executes the save() method on all existing LetterImages,
+    as this is needed to trigger the generation of thumbnail images
+    for all existing LetterImages as per the changes made in
+    migration: 0008_letterimage_image_thumbnail
+    """
+
+    for obj in models.LetterImage.objects.all():
+        obj.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('researchdata', '0008_letterimage_image_thumbnail'),
+    ]
+
+    operations = [
+        migrations.RunPython(trigger_save_letterimage)
+    ]
